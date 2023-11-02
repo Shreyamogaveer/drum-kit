db506d1b9f938a29b836b92f04f4cecf731cecda
Shreya S <136468875+Shreyamogaveer@users.noreply.github.com>
2023-11-02 11:58:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for(var i=0; i<loopNumber; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML=this.innerHTML;
         makeSound(buttonInnerHTML);
+         buttonAnimation(buttonInnerHTML);
 });
 
 }
@@ -14,6 +15,7 @@ for(var i=0; i<loopNumber; i++){
 
 document.addEventListener("keydown",function (event){
     makeSound(event.key);
+     buttonAnimation(event.key);
 });
 
 
@@ -49,5 +51,16 @@ function makeSound(key){
        }
 
 }
+
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed");
+    },100);
+
+}
+
 
 
